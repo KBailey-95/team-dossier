15ce389ff3766b984c32a8a342311e74d6099f67
Beast <95392136+KBailey-95@users.noreply.github.com>
2022-05-14 11:10:44 -0400

    creation of test files and completion of core functionality.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require('./lib/renderedHTML');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 const employees = [];
 
 //User Input
